7eca9c2f104e05d299645d49d81bfb0bdc439eb0
Stefan Eng <stefaneng13@gmail.com>
2013-08-26 12:36:36 -0700

    ListView now respondes to clicks

--- src/com/TimeStat/MainActivity.java
+++ src/com/TimeStat/MainActivity.java
@@ -1,9 +1,13 @@
 package com.TimeStat;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,19 +20,43 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-
         List<String> test = new ArrayList<String>();
         test.add("Hello");
         test.add("world");
+        test.add("This");
+        test.add("is");
+        test.add("a");
+        test.add("ListView");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, test);
         ListView listview = (ListView) findViewById(R.id.listview);
 
         listview.setAdapter(adapter);
 
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+                CharSequence text = "You pressed list position number: " + position;
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
         eventData = new EventsAccess(this);
 
         eventData.open();
         eventData.close();
     }
+
+    /* public void onClick(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }  */
 }
