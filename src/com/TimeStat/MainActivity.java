01abc947290bde70b63d71fc174a168be70906bb
Stefan Eng <stefaneng13@gmail.com>
2013-08-27 20:20:58 -0700

    Add activities button now work, rotating screen does not delete ListView

--- src/com/TimeStat/MainActivity.java
+++ src/com/TimeStat/MainActivity.java
@@ -2,6 +2,7 @@ package com.TimeStat;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,21 +16,26 @@ import java.util.List;
 public class MainActivity extends Activity {
     private EventsAccess eventData;
 
+    // ArrayList of strings for the events, get it from the database later
+    List<String> events = new ArrayList<String>();
+
+    // How we will display the event names on the ListView
+    private ArrayAdapter<String> adapter;
+
+    private ListView listview;
+
+
+    // Counter, delete when we are actually connecting to the database
+    private int count = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        List<String> test = new ArrayList<String>();
-        test.add("Hello");
-        test.add("world");
-        test.add("This");
-        test.add("is");
-        test.add("a");
-        test.add("ListView");
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, events);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, test);
-        ListView listview = (ListView) findViewById(R.id.listview);
+        listview = (ListView) findViewById(R.id.listview);
 
         listview.setAdapter(adapter);
 
@@ -51,12 +57,27 @@ public class MainActivity extends Activity {
         eventData.close();
     }
 
-    /* public void onClick(View view) {
+    /* There might be a better way to do this but this allows the ListView
+     * to stay when we rotate the screen.
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        listview.setAdapter(adapter);
+    }
+
+    /* Gets called when the user presses the add more activities button */
+    public void onClick(View view) {
+        adapter.add("Test Event " + count++);
+        listview.setAdapter(adapter);
+
+        // Toast
         Context context = getApplicationContext();
-        CharSequence text = "Hello toast!";
+        CharSequence text = "Added Event";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
-    }  */
+    }
 }
